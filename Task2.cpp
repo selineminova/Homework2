1c7c7dc053664b97b55acd265c2524a50b69765a
selineminova <32775976+selineminova@users.noreply.github.com>
2018-01-04 18:47:33 +0200

    Add files via upload

---
+++ Task2.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<fstream>
+#include<cstring>
+using namespace std;
+int main()
+{
+	const int size=6;
+	int matrix[size][size];
+	int i,j,sum;
+	for(i=0;i<size;i++)
+	{
+		for(j=0;j<size;j++)
+		{
+			cout<<"Enter element"<<i<<"."<<j<<":";
+			cin>>matrix[i][j];
+		}
+		
+	}
+	for(i=0;i<size;i++)
+	{
+		for(j=0;j<size;j++)
+		{
+			cout<<matrix[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	for(i=0;i<size;i++)
+	{
+		sum=0;
+		for(j=0;j<size;j++)
+		{
+			if(matrix[i][j]!=0)
+			{
+				sum=sum+matrix[i][j];
+			}
+		}
+		cout<<"Sum of the odd numbers in row"<<" "<<i+1<<" "<<"is"<<" "<<sum<<endl;
+	}
+	
+}
